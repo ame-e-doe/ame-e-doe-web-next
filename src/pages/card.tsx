af0805a8824c53b74540bc80766a49de1f062d85
Ricardo Dias <diasricardo2001@gmail.com>
2022-10-29 19:38:49 -0300

    finalização da tela adicionar cartão

--- src/pages/card.tsx
+++ src/pages/card.tsx
@@ -1,8 +1,9 @@
-import styles from "../../styles/home.module.scss";
+import styles from "../../styles/card.module.scss";
 import { useState, FormEvent } from "react";
 import { Input } from "../components/input";
 import { CreateCardDto } from "../dto/create-card-dto";
 import { setupApiClient } from "../services/api";
+import Header from "../components/header";
 
 export default function Card() {
   const [numCard, setNumCart] = useState("");
@@ -39,36 +40,55 @@ export default function Card() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.login}>
+      <Header />
+      <div className={styles.card}>
+        <h1> Cadastro de Cartão </h1>
         <form className={styles.form} action="" onSubmit={saveCard}>
           <Input
             placeholder="Número do cartão"
             type="text"
             value={numCard}
-            onChange={(e) => setNumCart(e.target.value)}
+            onChange={(e) => {
+              const numberPattern = /\d+/g;
+              const validacaoNumero = e.target.value.match(numberPattern) || [];
+              const value = validacaoNumero.join("");
+              setNumCart(value);
+            }}
           />
-
-          <span>
+          <div className={styles.cvv}>
             <Input
               placeholder="CVV"
               type="text"
               value={cvv}
-              onChange={(e) => setCvv(e.target.value)}
+              maxLength={3}
+              onChange={(e) => {
+                const numberPartter = /\d+/g;
+                const validateNumber =
+                  e.target.value.match(numberPartter) || [];
+                const value = validateNumber.join("");
+                setCvv(value);
+              }}
             />
+
             <Input
               placeholder="Validade"
               type="text"
               value={validate}
               onChange={(e) => setValidate(e.target.value)}
             />
-          </span>
+          </div>
+
           <Input
             placeholder="Nome"
             type="text"
             value={name}
-            onChange={(e) => setName(e.target.value)}
+            onChange={(e) => {
+              const numberPartter = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/;
+              const validateNumber = e.target.value.match(numberPartter) || [];
+              const value = validateNumber.join("");
+              setName(value);
+            }}
           />
-
           <button type="submit">Salvar cartão</button>
         </form>
       </div>
