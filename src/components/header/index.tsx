563bf86a0d4eb392f100b12983231fae9f68ac32
Ricardo Dias <diasricardo2001@gmail.com>
2022-11-05 23:17:07 -0300

    Alteracao do header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,69 @@
 import styles from "./styles.module.scss";
 import Link from "next/link";
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import Box from '@mui/material/Box';
+import {useState} from 'react'; 
+import Router from "next/router";
+
 
 export default function Header() {
+
+  const [auth, setAuth] = useState(true);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleAccount = () => {
+    Router.push('/userEdit');
+    setAnchorEl(null);
+  };
+
+  const handleClose = () => {
+    Router.push('#');
+    setAnchorEl(null);
+  };
+
   return (
     <div className={styles.header}>
       <h3>AME E DOE</h3>
       <div className={styles.botoesHeader}>
-        <Link href="/login">ENTRAR</Link>
-        <Link href="/cadastro">CADASTRE-SE</Link>
+        <span>Olá, Fulano</span>
+        <Box>
+          {auth && (
+            <div>
+              <IconButton 
+                onClick={handleMenu}
+                color="inherit"
+              >
+              <AccountCircle sx={{ fontSize: 40}}/>
+              </IconButton>
+              <Menu 
+                sx={{ mt: '35px'}}
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+              <MenuItem onClick={handleAccount}>Minha Conta</MenuItem>
+              <MenuItem onClick={handleClose}>Sair</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Box>
       </div>
     </div>
   );
