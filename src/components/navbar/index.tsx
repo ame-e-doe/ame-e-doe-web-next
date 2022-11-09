7747988cd054a2dd75f88be09305d504a0002c10
Rennan Costa <rennansilvacosta@hotmail.com>
2022-11-09 19:21:04 -0300

    alterando a cor do menu de acordo com a rota

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,19 +1,53 @@
 import styles from "./styles.module.scss";
 import Link from "next/link";
+import React from "react";
+
+import { useRouter } from "next/router";
 
 export default function Nav() {
   //pesquisar como fazer com Next (converter a Dom)
   //const titleCurrent = document.title;
+
+  const router = useRouter();
+
   return (
     <div className={styles.nav}>
       <div className={styles.botoesNav}>
-        <Link href="/userEdit">Detalhes</Link>
+        <Link
+          style={{
+            color: router.pathname == "/userEdit" ? "#e91c5d" : "#FFF",
+          }}
+          href="/userEdit"
+        >
+          Detalhes
+        </Link>
 
-        <Link href="/myImages">Minhas Imagens</Link>
+        <Link
+          style={{
+            color: router.pathname == "/myImages" ? "#e91c5d" : "#FFF",
+          }}
+          href="/myImages"
+        >
+          Minhas imagens
+        </Link>
 
-        <Link href="/card">Adicionar cartão</Link>
+        <Link
+          style={{
+            color: router.pathname == "/card" ? "#e91c5d" : "#FFF",
+          }}
+          href="/card"
+        >
+          Adicionar cartão
+        </Link>
 
-        <Link href="/passwordEdit">Alterar senha</Link>
+        <Link
+          style={{
+            color: router.pathname == "/passwordEdit" ? "#e91c5d" : "#FFF",
+          }}
+          href="/passwordEdit"
+        >
+          Alterar senha
+        </Link>
       </div>
     </div>
   );
