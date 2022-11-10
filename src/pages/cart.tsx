0f7560acd9aee8facbbb91044341206962b6a201
Rennan Costa <rennan.costa@pcivil.rj.gov.br>
2022-11-10 00:15:17 -0300

    listando itens do carrinho, removendo item do carrinho pelo id, finalizando venda

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -2,24 +2,88 @@ import { CartType } from "../models/cart-type";
 import { setupApiClient } from "../services/api";
 import { canSSRAuth } from "../utils/canSSRAuth";
 import { useState } from "react";
+import { Product } from "../models/product-type";
+import { CreateSaleDto } from "../dto/create-sale-dto";
+import Router from "next/router";
+import { toast } from "react-toastify";
+import { CartItem } from "../models/cart-item-type";
 
-export default function Cart(c: CartType) {
-  const [cart, setCart] = useState(c || []);
+export interface ShoppingCart {
+  cart: CartType;
+}
+
+export default function Cart({ cart }: ShoppingCart) {
+  const [itens, setItens] = useState<CartType>(cart);
+  const [cartItens, setCartItens] = useState<CartItem[]>(itens.cartItems);
+
+  let listProducts: Product[] = [];
+
+  itens.cartItems.forEach((i) => {
+    listProducts.push(i.product);
+  });
 
   const api = setupApiClient();
 
+  //finaliza o pedido
+  async function finishOrder() {
+    const sale: CreateSaleDto = {
+      value: itens.total,
+      products: listProducts.map((p) => {
+        return p.id;
+      }),
+    };
+    await api
+      .post("/sales/create", sale)
+      .then(function (response) {
+        toast.success("Compra finalizada com sucesso.");
+        Router.push("/myImages");
+        console.log(response);
+      })
+      .catch(function (error) {
+        toast.error("Algo deu errado, tente novamente mais tarde.");
+        Router.push("/");
+        console.log(error);
+      });
+  }
+
   //remove item do carrinho
-  async function removeItem(idItem: number) {
-    await api.delete("/cart/", {
-      data: {
-        idItem,
-      },
-    });
+  async function removeItem(itemId: number) {
+    await api
+      .delete(`/cart/${itemId}`)
+      .then(function (response) {
+        toast.success("Item removido com sucesso");
+
+        setCartItens(
+          itens.cartItems.filter((i) => {
+            return i.id !== itemId;
+          })
+        );
+
+        console.log(response);
+      })
+      .catch(function (error) {
+        toast.error("Algo deu errado, tente novamente mais tarde.");
+        console.log(error);
+      });
   }
 
   return (
     <div>
       <h1>Aqui vou exibir o carrinho</h1>
+
+      <div style={{ color: "#fff" }}>
+        {cartItens.map((item) => (
+          <section key={item.id}>
+            <p>{item.price}</p>
+            <p>{item.product.title}</p>
+            <p>{item.product.description}</p>
+            <p>{item.product.value}</p>
+          </section>
+        ))}
+      </div>
+
+      <button onClick={finishOrder}> Finalizar Venda </button>
+      <button onClick={() => removeItem(18)}>Remove produto do carrinho</button>
     </div>
   );
 }
