5e0962e57a236be341bf9878603356bc17bb9b08
Andrew Nascimento <81823870+AndrewPolengolas@users.noreply.github.com>
2022-11-10 11:11:06 -0300

    Exclusao de imagens

--- src/components/carrossel/carrossel.tsx
+++ src/components/carrossel/carrossel.tsx
@@ -1,15 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import Carousel  from 'react-bootstrap/Carousel';
-import imagem1 from "../../assets/imagem1.png";
-import imagem2 from "../../assets/imagem2.png";
-import imagem3 from "../../assets/imagem3.png";
-import imagem4 from "../../assets/imagem4.png";
 import styles from "./styles.module.scss";
   
 export default function Carrossel() {
   return (
     <div className={styles.container}>
-      <Carousel>
+      {/* <Carousel>
         <Carousel.Item>
           <img 
             className={styles.item}
@@ -30,7 +26,7 @@ export default function Carrossel() {
             className={styles.item}
             src={imagem4.src} alt="imagem"/>
         </Carousel.Item>
-      </Carousel>
+      </Carousel> */}
     </div>
   );
 }
