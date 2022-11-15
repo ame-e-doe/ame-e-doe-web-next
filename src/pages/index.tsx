54e09ec708a1900147dbf8869315f578a61c3e7a
Andrew Nascimento <81823870+AndrewPolengolas@users.noreply.github.com>
2022-11-15 12:59:12 -0300

    Tela home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ import { setupApiClient } from "../services/api";
 import { canSSRAuth } from "../utils/canSSRAuth";
 import { Product } from "../models/product-type";
 import Header from "../components/header";
+import { ImageList, ImageListItem, ImageListItemBar, Link } from "@mui/material";
+import { useRouter } from "next/router";
+import { Details } from "@mui/icons-material";
 
 interface ProductList {
   listProduct: Product[];
@@ -12,13 +15,83 @@ interface ProductList {
 export default function Home({ listProduct }: ProductList) {
   const [products, setProducts] = useState(listProduct || []);
 
+  const router = useRouter();
+
+  let listProducts: Product[] = [];
+
+  if (products.length > 0) {
+    products.forEach((s) => {
+      listProducts.push(s);
+    });
+  }
+  
   return (
     <div className={styles.Container}>
       <Header />
       <div className={styles.logoLove}>
         <h1>AME E DOE</h1>
       </div>
-      <div className={styles.boxCarousel}></div>
+      <div className={styles.boxCarousel}>
+      <div className={styles.myImages}>
+        <div
+          style={{
+            padding: "2rem",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <ImageList
+            style={{
+              gap: "3rem",
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+            }}
+            sx={{ width: "100%" }}
+          >
+            {listProducts.map((item) => {
+              console.log(item);
+              return (
+                <ImageListItem
+                  style={{ height: "250px", maxWidth: "250px" }}
+                  key={item.id}
+                >
+                  <img
+                    style={{
+                      width: "250px",
+                      height: "100%",
+                      borderRadius: "15px",
+                    }}
+                    src={item.image.url}
+                    alt={item.title}
+                  />
+                  <ImageListItemBar
+                    title={
+                      <div
+                        style={{ height: "30px", whiteSpace: "break-spaces" }}
+                      >
+                        <Link
+                          style={{
+                            color:
+                              router.pathname == "/detalhes"
+                                ? "#e91c5d"
+                                : "#FFF",
+                          }}
+                          href={"/detalhes/" + item.id}
+                        >
+                          {item.title}
+                        </Link>
+                      </div>
+                    }
+                  />
+                </ImageListItem>
+              );
+            })}
+          </ImageList>
+        </div>
+      </div>
+      </div>
     </div>
     
   );
