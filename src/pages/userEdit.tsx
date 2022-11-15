ac828aa51fe9c7952002564fcf50804356e72cea
Rennan Costa <rennan.costa@pcivil.rj.gov.br>
2022-11-15 11:02:58 -0300

    alterando dados do user

--- src/pages/userEdit.tsx
+++ src/pages/userEdit.tsx
@@ -1,24 +1,49 @@
 import { FormEvent, useContext, useState } from "react";
-import { AuthContext } from "../contexts/auth-context";
 import Header from "../components/header";
 import Nav from "../components/navbar";
 import styles from "../../styles/userEdit.module.scss";
 import { Input } from "../components/input";
-import Link from "next/link";
 import Head from "next/head";
+import { setupApiClient } from "../services/api";
+import { UserEditDto } from "../dto/user-edit-dto";
+import { toast } from "react-toastify";
+import Router from "next/router";
+import { destroyCookie } from "nookies";
 
 export default function UserEdit() {
-  const { signIn } = useContext(AuthContext);
-
-  const [name, setName] = useState("");
-  const [sobrenome, setSobrenome] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   async function handleEditUser(event: FormEvent) {
     event.preventDefault();
-    const credentials = {
-      name,
-      sobrenome,
+
+    const api = setupApiClient();
+
+    const edit: UserEditDto = {
+      firstName,
+      lastName,
     };
+
+    await api
+      .put("user/update", edit)
+      .then((response) => {
+        toast.success("Dados alterados com sucesso");
+        cleanFiles();
+        destroyCookie(undefined, "@nextauth.token");
+        destroyCookie(undefined, "@nextauth.id");
+        Router.push("/login");
+        console.log(response);
+      })
+      .catch((err) => {
+        toast.error("Algo deu errado, tente novamente mais tarde.");
+        cleanFiles();
+        console.log(err);
+      });
+  }
+
+  function cleanFiles() {
+    setFirstName("");
+    setLastName("");
   }
 
   return (
@@ -36,15 +61,15 @@ export default function UserEdit() {
           <Input
             placeholder="Nome"
             type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
           />
 
           <Input
             placeholder="Sobrenome"
             type="text"
-            value={sobrenome}
-            onChange={(e) => setSobrenome(e.target.value)}
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
           />
 
           <button type="submit"> Salvar </button>
